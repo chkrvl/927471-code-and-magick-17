16fb884f1a673d0dfec20f78afc0404bf0e131ab
kirill <chkrvl@gmail.com>
2019-06-11 13:06:31 +0400

    Добавил функцию renderHystogram

--- js/stat.js
+++ js/stat.js
@@ -39,6 +39,21 @@ var paintStatisticBar = function (namePlayer) {
   }
 
   return colorBar;
+};
+
+var renderHystogram = function (ctx, namePlayer, timePlayer) {
+  var maxTime = getMaxElement(timePlayer);
+
+  for (var i = 0; i < namePlayer.length; i++) {
+    var time = Math.round(timePlayer[i]);
+    var columnHeight = (time * COLUMN_MAX_HEIGHT) / maxTime;
+
+    ctx.fillStyle = paintStatisticBar(namePlayer[i]);
+    ctx.fillRect(CLOUD_X + COLUMN_GAP + (COLUMN_GAP + COLUMN_WIDTH) * i, CLOUD_HEIGHT - FONT_SIZE - GAP, COLUMN_WIDTH, -columnHeight);
+    ctx.fillStyle = '#000';
+    ctx.fillText(namePlayer[i], CLOUD_X + COLUMN_GAP + (COLUMN_GAP + COLUMN_WIDTH) * i, CLOUD_HEIGHT - GAP);
+    ctx.fillText(time, CLOUD_X + COLUMN_GAP + (COLUMN_GAP + COLUMN_WIDTH) * i, CLOUD_HEIGHT - FONT_SIZE - GAP - FONT_GAP - columnHeight);
+  }
 }
 
 window.renderStatistics = function (ctx, names, times) {
@@ -51,16 +66,5 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура вы победили!', CLOUD_X + FONT_GAP, CLOUD_Y + FONT_GAP);
   ctx.fillText('Список результатов:', CLOUD_X + FONT_GAP, CLOUD_Y + FONT_GAP * 2);
 
-  var maxTime = getMaxElement(times);
-
-  for (var i = 0; i < names.length; i++) {
-    var time = Math.round(times[i]);
-    var columnHeight = (time * COLUMN_MAX_HEIGHT) / maxTime;
-
-    ctx.fillStyle = paintStatisticBar(names[i]);
-    ctx.fillRect(CLOUD_X + COLUMN_GAP + (COLUMN_GAP + COLUMN_WIDTH) * i, CLOUD_HEIGHT - FONT_SIZE - GAP, COLUMN_WIDTH, -columnHeight);
-    ctx.fillStyle = '#000';
-    ctx.fillText(names[i], CLOUD_X + COLUMN_GAP + (COLUMN_GAP + COLUMN_WIDTH) * i, CLOUD_HEIGHT - GAP);
-    ctx.fillText(time, CLOUD_X + COLUMN_GAP + (COLUMN_GAP + COLUMN_WIDTH) * i, CLOUD_HEIGHT - FONT_SIZE - GAP - FONT_GAP - columnHeight);
-  }
+  renderHystogram(ctx, names, times);
 };
