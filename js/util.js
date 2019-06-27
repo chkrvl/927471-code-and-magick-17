90714f9553fdda3ec2820dd29ff71b9e5647992d
kirill <chkrvl@gmail.com>
2019-06-27 12:28:24 +0400

    Добавил модуль util.js

---
+++ js/util.js
@@ -0,0 +1,18 @@
+'use strict';
+window.util = (function () {
+  var ESC_KEYCODE = 27;
+  var ENTER_KEYCODE = 13;
+
+  return {
+    isEscEvent: function (evt, action, limitation) {
+      if (evt.keyCode === ESC_KEYCODE && document.activeElement !== limitation) {
+        action();
+      }
+    },
+    isEnterEvent: function (evt, action) {
+      if (evt.keyCode === ENTER_KEYCODE) {
+        action();
+      }
+    }
+  };
+})();
