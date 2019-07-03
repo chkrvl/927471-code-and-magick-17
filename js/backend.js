64296339a050e79db013ded7397a4c0736cf0cd3
kirill <chkrvl@gmail.com>
2019-07-03 13:35:22 +0400

    Добавил обработчик ошибок в save()

--- js/backend.js
+++ js/backend.js
@@ -2,6 +2,33 @@
 (function () {
   var URL = 'https://js.dump.academy/code-and-magick';
 
+  // var setXHR = function (onLoad, onError, data, timeout, requestMethod, requestURL) {
+  //   var xhr = new XMLHttpRequest();
+  //   xhr.responseType = 'json';
+
+  //   xhr.addEventListener('load', function () {
+  //     if (xhr.status === 200) {
+  //       onLoad(xhr.response);
+  //     } else {
+  //       onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+  //     }
+  //   });
+  //   xhr.addEventListener('error', function () {
+  //     onError('Произошла ошибка соединения');
+  //   });
+  //   xhr.addEventListener('timeout', function () {
+  //     onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+  //   });
+
+  //   xhr.timeout = timeout;
+
+  //   xhr.open(requestMethod, requestURL);
+  //   xhr.send(data);
+  // };
+
+  // var load = setXHR(undefined, undefined, undefined, 10000, 'GET', URL + '/data');
+  // var load = setXHR(undefined, undefined, undefined, 10000, 'POST', URL);
+
   var load = function (onLoad, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
@@ -37,6 +64,14 @@
         onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
       }
     });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000; // 10s
 
     xhr.open('POST', URL);
     xhr.send(data);
